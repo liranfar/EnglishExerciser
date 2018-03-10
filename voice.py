c9a9a52597623d4b4f35eb9d79b94a3b6fe4480b
liranfar <l.farage@gmail.com>
2018-03-10 23:51:34 +0200

    added timeout exception for voice asynchronous task

--- voice.py
+++ voice.py
@@ -1,20 +1,44 @@
 import os
+import signal
+from contextlib import contextmanager
 
 
-class Voice():
-
+class Voice:
     @classmethod
     def say(cls, sentence):
 
         if sentence is None:
             return
 
-        from gtts import gTTS
-        tts = gTTS(text=sentence, lang='en')
-        temp_sound = '/tmp/temp.mp3'
-        tts.save(temp_sound)
-        from pygame import mixer
-        mixer.init()
-        mixer.music.load(temp_sound)
-        mixer.music.play()
-        os.remove(temp_sound)
+        try:
+            with time_limit(2):
+
+                from gtts import gTTS
+
+                tts = gTTS(text=sentence, lang='en')
+                temp_sound = '/tmp/temp.mp3'
+                tts.save(temp_sound)
+                from pygame import mixer
+
+                mixer.init()
+                mixer.music.load(temp_sound)
+                mixer.music.play()
+                os.remove(temp_sound)
+        except TimeoutException as e:
+            print("Timed out!")
+
+
+class TimeoutException(Exception): pass
+
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
